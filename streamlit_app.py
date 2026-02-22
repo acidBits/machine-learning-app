ee5416a75d489ad8cae40a6ff00d94504a5944fe
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-22 05:22:54 -0300

    Fix formatting issues in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title('Machine Learning App')
 st.info('This app buils a machine learning model!')
 
- df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
- st.write(df.head(10))
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+st.write(df.head(10))
