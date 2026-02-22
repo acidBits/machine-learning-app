ac3855e0d5a9ba586d3d948658d36f385cc43a73
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-22 06:38:03 -0300

    Fix slider values for bill length in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ with st.sidebar:
   st.header('Entrada de Atributos')
   island = st.selectbox('Island:', df['island'].unique())
   gender = st.selectbox('Gender:', df['sex'].unique())
-  bill_length_mm = st.slider('Bill length(mm):', 32.10, 59.60, 43)
+  bill_length_mm = st.slider('Bill length(mm):', 32.1, 59.6, 43)
   bill_depth_mm = st.slider('Bill depth(mm):',13.10, 25.50, 17.16)
   flipper_length_mm = st.slider('Flipper length(mm):',181,231,205)
   body_mass_g = st.slider('body_mass(g):', 2700, 6300, 4000)
