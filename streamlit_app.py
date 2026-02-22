ed6574e6674352268bdd238535ff6e12c15442c0
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-22 05:28:39 -0300

    Change expander title from 'Data' to 'Dados'

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('Machine Learning App')
 st.info('This app buils a machine learning model!')
 
-with st.expander('Data'):
+with st.expander('Dados'):
   st.write('**Dados Brutos**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
