9262b68ae233583620537b1d81d5af827fc16a01
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-22 05:22:14 -0300

    Display first 10 rows of the DataFrame

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title('Machine Learning App')
 st.info('This app buils a machine learning model!')
 
  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
- df
+ st.write(df.head(10))
