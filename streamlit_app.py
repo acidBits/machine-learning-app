804c3e3876878c4ee755df74632e08c1c5879418
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-22 06:21:45 -0300

    Update body mass slider range and label

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,4 @@ with st.sidebar:
   bill_length_mm = st.slider('Bill length(mm):', df['bill_length_mm'].min(), df['bill_length_mm'].max(), df['bill_length_mm'].mean())
   bill_depth_mm = st.slider('Bill depth(mm):', df['bill_depth_mm'].min(), df['bill_depth_mm'].max(), df['bill_depth_mm'].mean())
   
-  body_mass_g = st.slider('body_mass(g)', df['body_mass_g'].min(), df['body_mass_g'].max(), df['body_mass_g'].mean())
+  body_mass_g = st.slider('body_mass(g):', 0,10, 5)
