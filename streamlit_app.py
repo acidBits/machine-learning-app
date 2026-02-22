83afd25c82864da8cd838bd52b8ddef08d1763a1
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-22 05:27:59 -0300

    Fix expander syntax in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('Machine Learning App')
 st.info('This app buils a machine learning model!')
 
-with expander('Data'):
+with st.expander('Data'):
   st.write('**Dados Brutos**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
