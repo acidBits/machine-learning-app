3b8e757a78f224d358e73d54b7113d7d31ab260b
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-22 06:33:06 -0300

    Update sliders with fixed values for measurements

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ with st.sidebar:
   st.header('Entrada de Atributos')
   island = st.selectbox('Island:', df['island'].unique())
   gender = st.selectbox('Gender:', df['sex'].unique())
-  bill_length_mm = st.slider('Bill length(mm):', df['bill_length_mm'].min(), df['bill_length_mm'].max(), df['bill_length_mm'].mean())
-  bill_depth_mm = st.slider('Bill depth(mm):', df['bill_depth_mm'].min(), df['bill_depth_mm'].max(), df['bill_depth_mm'].mean())
-  
+  bill_length_mm = st.slider('Bill length(mm):', 32.10, 59.60, 43)
+  bill_depth_mm = st.slider('Bill depth(mm):',13.10, 25.50, 17.16)
+  Flipper_length_mm = st.slider('Flipper length(mm)',181,231,205)
   body_mass_g = st.slider('body_mass(g):', 2700, 6300, 4000)  
