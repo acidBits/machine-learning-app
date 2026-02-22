626236a165aa5f47baee4938629b7657775195c9
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-22 05:38:17 -0300

    Integrate data loading and visualization features
    Added data visualization with scatter chart for penguins dataset.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
 st.title('Machine Learning App')
 st.info('This app buils a machine learning model!')
 
 with st.expander('Dados'):
   st.write('**Dados Brutos**')
-  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+
+with st.expander('Visualizacao de Dados'):
+  st.scatter_chart(df,x='bill_length_mm',y='body_mass_g',color='species')
