6d8d46f40e4acf699f375500eefa68c58d163738
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-22 05:21:32 -0300

    Load and display penguins dataset
    Added functionality to load and display a dataset.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title('Machine Learning App')
 st.info('This app buils a machine learning model!')
+
+ df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+ df
