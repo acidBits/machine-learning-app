47efbd44444224dd63cb53732a434ea5d82e302b
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-23 02:51:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ with st.expander('Visualizacao de Dados'):
                    x='bill_length_mm', 
                    y='body_mass_g', 
                    color='species',
-                   y_label = 'massa corporal(g)
+                   y_label = 'massa corporal(g)'
                   )
 
 with st.sidebar:
