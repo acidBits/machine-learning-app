d44d584a26a7324689b887cbb8fd6a23521da419
acidBits <43962792+acidBits@users.noreply.github.com>
2026-02-23 02:58:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ with st.expander('Visualizacao de Dados'):
                    y='body_mass_g', 
                    color='species',
                    y_label = 'massa corporal (g)',
-                   x_label = 'comprimento bico (mm)'
+                   x_label = 'comprimento bico (mm)',
                    color_label = 'Especies'
                   )
 
